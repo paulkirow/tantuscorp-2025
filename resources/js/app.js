ce6fd0c17f012931fb727465e3315aecbde5403c
Paul Kirow <kirow1993@gmail.com>
2025-07-13 23:23:26 -0400

    include bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
+import '../sass/app.scss';
+
 import './bootstrap';
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+                'resources/sass/app.scss',
+            ],
             refresh: true,
         }),
     ],
+    css: {
+        devSourcemap: true,
+    },
 });
